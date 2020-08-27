f12d9c5b7c86c22db683707855fd4ef9744e898d
sa30nitk <sa30nitk@gmail.com>
2020-08-27 10:48:37 +0530

    http-client with core request

--- src/mtlslearning_clojure_client/core.clj
+++ src/mtlslearning_clojure_client/core.clj
@@ -1,8 +1,12 @@
 (ns mtlslearning-clojure-client.core
   (:gen-class)
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clj-http.core :as client-core]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (client/get "http://localhost:8080/ping"))
+  ;(client/get "http://localhost:8080/ping")
+  (client-core/request {:request-method :get
+                        :scheme :http
+                        :uri "localhost:8080/ping"}))
