52be6db2aa85aa33006aeb031a7e1cd4e0be407d
sa30nitk <sa30nitk@gmail.com>
2020-08-27 10:12:01 +0530

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mtlslearning-clojure-client
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mtlslearning_clojure_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns mtlslearning-clojure-client.core-test
+  (:require [clojure.test :refer :all]
+            [mtlslearning-clojure-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
